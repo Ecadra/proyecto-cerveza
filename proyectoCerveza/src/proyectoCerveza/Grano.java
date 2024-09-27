ebf5ae90c2988c4280ec6fdf9d38a066391296c8
Sebastian <usgg1106@gmail.com>
2024-09-27 16:30:58 -0500

    GRANO (SIN ERRORES) PEDIDO (AUN NO TESTEADO) RECETA (AUN NO TESTEADO)

---
+++ proyectoCerveza/src/proyectoCerveza/Grano.java
@@ -0,0 +1,71 @@
+
+package proyectoCerveza;
+import java.io.Serializable;
+import javax.persistence.*;
+import java.util.*;
+import proyectoCerveza.Cerveza;
+
+/**
+ *
+ * @author ulseg
+ */
+@Entity
+public class Grano implements Serializable{
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    private String gra_nombre;
+    private String gra_procedencia;
+            
+    @ManyToOne
+    @JoinColumn(name = "rec_gra", nullable = false)
+    private Receta gra_rec;
+            
+    public Grano(){
+        this.gra_nombre = "";
+        this.gra_procedencia = "";
+    }
+    
+    public Grano(String nom, String proc){
+        this.gra_nombre = nom;
+        this.gra_procedencia = proc;
+    }
+    
+    @Override
+    public String toString() {
+        return String.format("\n-----\nNombre del grano %s "
+                + "\nProcedencia del grano: %s \n",
+                this.gra_nombre, this.gra_procedencia);
+    }
+    
+    public void formGra_rec(Receta r1) {
+        gra_rec = r1;
+    }
+
+    public String getGra_nombre() {
+        return gra_nombre;
+    }
+    
+    public void setGra_nombre(String gra_nombre) {
+        this.gra_nombre = gra_nombre;
+    }
+
+    public String getGra_procedencia() {
+        return gra_procedencia;
+    }
+
+    public void setGra_procedencia(String gra_procedencia) {
+        this.gra_nombre = gra_procedencia;
+    }
+
+
+    public Receta getGra_rec() {
+        return gra_rec;
+    }
+
+    public void setGra_rec(Receta gra_rec) {
+        this.gra_rec = gra_rec;
+    }
+
+ 
+}
---
+++ proyectoCerveza/src/proyectoCerveza/Receta.java
@@ -0,0 +1,85 @@
+
+package proyectoCerveza;
+import java.io.Serializable;
+import javax.persistence.*;
+import java.util.*;
+import proyectoCerveza.Grano;
+import proyectoCerveza.Cerveza;
+
+/**
+ *
+ * @author ulseg
+ */
+@Entity
+public class Receta implements Serializable{
+
+    @Id
+    private String rec_procedimiento;
+   
+    @OneToMany
+    @JoinColumn(name="gra_rec",nullable=false)
+    private List<Grano> rec_gra=new ArrayList<Grano>();
+    
+    @OneToMany
+    @JoinColumn(name="cer_rec",nullable=false)
+    private List<Cerveza> rec_cer=new ArrayList<Cerveza>();
+    
+    public Receta(){
+        this.rec_procedimiento = "";
+    }
+    
+    public Receta(String nom){
+        this.rec_procedimiento = nom;
+    }
+    
+    
+    @Override
+    public String toString() {
+        return String.format("\n-----\nProcedimiento: %s "
+                + "\nGrano: %s "
+                + "\nCerveza: %s \n",
+                this.rec_procedimiento, this.getRec_gra(), this.getRec_cer());
+    }
+    
+    public void printGranos(){
+        System.out.println("Salas: "+getRec_gra().size());
+        
+        for (int i=0;i<getRec_gra().size();i++) {
+            System.out.println(getRec_gra().get(i));
+        }
+    }
+    
+    public void printCervezas(){
+        System.out.println("Salas: "+getRec_cer().size());
+        
+        for (int i=0;i<getRec_cer().size();i++) {
+            System.out.println(getRec_cer().get(i));
+        }
+    }
+    
+    public void formRec_gra(Grano g1)
+    {
+        getRec_gra().add(g1);
+    }
+    
+    public void formRec_cer(Cerveza c1)
+    {
+        getRec_cer().add(c1);
+    }    
+    
+    public String getRec_procedimientgo() {
+        return rec_procedimiento;
+    }
+    
+    public void setRec_procedimiento(String rec_procedimiento) {
+        this.rec_procedimiento = rec_procedimiento;
+    }
+    
+    public List<Grano> getRec_gra() {
+        return rec_gra;
+    }
+    
+    public List<Cerveza> getRec_cer() {
+        return rec_cer;
+    }
+}
